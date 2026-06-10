ee9aafc7b7a910930c1a1f98762cd648d224069b
Timileyin27 <timileyinabdulazeez27@gmail.com>
2026-06-10 14:45:55 +0100

    trying to connect

--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from app.database import engine,Base
 from app.routers import user,login
 Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 app= FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ai-azeez.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user.router)
 app.include_router(login.router)
 
